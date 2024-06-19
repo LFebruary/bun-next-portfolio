af6b27fd0f6adaca924adfce16525dd4ed028245
Lyle February <58416136+LFebruary@users.noreply.github.com>
2024-06-20 00:54:23 +0200

    Added focus hover state to work experience section

--- components/home/work-experience-section/work-experience-section.tsx
+++ components/home/work-experience-section/work-experience-section.tsx
@@ -2,14 +2,23 @@ import CoolText from "@/components/cool-text";
 import WorkExperienceTimeline from "@/components/timeline/work-experience-timeline";
 import WorkExperienceTimelineItem from "@/components/timeline/work-experience-timeline-item";
 import { WorkExperience } from "@/interfaces";
-import { FC } from "react";
+import { FC, useState } from "react";
+import { useInView } from "react-intersection-observer";
 
 const WorkExperienceSection: FC<{ workExperiences: WorkExperience[] }> = ({ workExperiences }) => {
+    const [inViewState, setInViewState] = useState(false);
+
+    const [ref] = useInView({
+        onChange: (inView) => setInViewState(inView),
+        threshold: 0.25,
+    });
+
     return (
         <>
             <CoolText
-                text="Work experience" />
-            <div style={{ marginInline: 128 }}>
+                text="Work experience"
+                forcedHoverState={inViewState} />
+            <div style={{ marginInline: 128 }} ref={ref}>
                 <WorkExperienceTimeline items={workExperiences.map((workExperience, index) =>
                     <WorkExperienceTimelineItem key={index}
                         startDate={workExperience.startDate}
