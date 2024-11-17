36c56550189140fd20148ab8069b3a138ef2f7b1
Lyle February <58416136+LFebruary@users.noreply.github.com>
2024-11-17 02:37:15 +0200

    Update personal-projects-section.tsx

--- components/home/personal-projects-section/personal-projects-section.tsx
+++ components/home/personal-projects-section/personal-projects-section.tsx
@@ -63,8 +63,8 @@ const ProjectsSection: FC<{ projects: Project[] }> = ({ projects }) => {
     );
 };
 
-function debounce<T extends (...args: any[]) => void>(func: T, delay: number): T {
-    let timeoutId: NodeJS.Timeout;
+function debounce<T extends (...args: unknown[]) => void>(func: T, delay: number): T {
+    let timeoutId: ReturnType<typeof setTimeout>;
     return ((...args: Parameters<T>) => {
         clearTimeout(timeoutId);
         timeoutId = setTimeout(() => func(...args), delay);
