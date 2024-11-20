5d4d7e6e28fac76ccf92752b907470b1e096a408
Lyle February <58416136+LFebruary@users.noreply.github.com>
2024-11-20 02:27:05 +0200

    Fix dependency issue

--- components/home/work-projects-section/work-projects-section.tsx
+++ components/home/work-projects-section/work-projects-section.tsx
@@ -1,65 +1,62 @@
 import CoolText from '@/components/cool-text';
-import WorkProjectCard from '@/components/work-project-card';
 import { WorkProject } from '@/interfaces/workProject.interface';
-import debounce from '@/utils/debounce';
-import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
-import { FC, memo, useCallback, useEffect, useMemo, useState } from 'react';
+import { FC, memo, useState } from 'react';
 import { useInView } from 'react-intersection-observer';
 
 const WorkProjectsSection: FC<{ projects: WorkProject[] }> = memo(({ projects }) => {
     const [inViewState, setInViewState] = useState(false);
-    const [maxDescriptionHeight, setMaxDescriptionHeight] = useState<number>(0);
-    const [maxTechSectionHeight, setMaxTechSectionHeight] = useState<number>(0);
+    // const [maxDescriptionHeight, setMaxDescriptionHeight] = useState<number>(0);
+    // const [maxTechSectionHeight, setMaxTechSectionHeight] = useState<number>(0);
 
     const [ref] = useInView({
         onChange: setInViewState,
         threshold: 0.6,
     });
 
-    const calculateMaxDescriptionHeight = useCallback(() => {
-        if (projects.length > 0) {
-            const maxHeight = projects.reduce((maxHeight, project) => {
-                const descriptionElement = document.getElementById(`description-${project.name}`);
-                return descriptionElement
-                    ? Math.max(maxHeight, descriptionElement.clientHeight)
-                    : maxHeight;
-            }, 0);
+    // const calculateMaxDescriptionHeight = useCallback(() => {
+    //     if (projects.length > 0) {
+    //         const maxHeight = projects.reduce((maxHeight, project) => {
+    //             const descriptionElement = document.getElementById(`description-${project.name}`);
+    //             return descriptionElement
+    //                 ? Math.max(maxHeight, descriptionElement.clientHeight)
+    //                 : maxHeight;
+    //         }, 0);
 
-            setMaxDescriptionHeight(maxHeight);
-        }
-    }, [projects]);
+    //         setMaxDescriptionHeight(maxHeight);
+    //     }
+    // }, [projects]);
 
-    const calculateMaxTechSectionHeight = useCallback(() => {
-        if (projects.length > 0) {
-            const maxHeight = projects.reduce((maxHeight, project) => {
-                const techSectionElement = document.getElementById(`technologies-${project.name}`);
-                return techSectionElement
-                    ? Math.max(maxHeight, techSectionElement.clientHeight)
-                    : maxHeight;
-            }, 0);
+    // const calculateMaxTechSectionHeight = useCallback(() => {
+    //     if (projects.length > 0) {
+    //         const maxHeight = projects.reduce((maxHeight, project) => {
+    //             const techSectionElement = document.getElementById(`technologies-${project.name}`);
+    //             return techSectionElement
+    //                 ? Math.max(maxHeight, techSectionElement.clientHeight)
+    //                 : maxHeight;
+    //         }, 0);
 
-            setMaxTechSectionHeight(maxHeight);
-        }
-    }, [projects]);
+    //         setMaxTechSectionHeight(maxHeight);
+    //     }
+    // }, [projects]);
 
-    const debouncedCalculate = useMemo(
-        () =>
-            debounce(() => {
-                calculateMaxDescriptionHeight();
-                calculateMaxTechSectionHeight();
-            }, 300),
-        [calculateMaxDescriptionHeight, calculateMaxTechSectionHeight]
-    );
+    // const debouncedCalculate = useMemo(
+    //     () =>
+    //         debounce(() => {
+    //             calculateMaxDescriptionHeight();
+    //             calculateMaxTechSectionHeight();
+    //         }, 300),
+    //     [calculateMaxDescriptionHeight, calculateMaxTechSectionHeight]
+    // );
 
-    useEffect(() => {
-        debouncedCalculate();
-        window.addEventListener('resize', debouncedCalculate);
+    // useEffect(() => {
+    //     debouncedCalculate();
+    //     window.addEventListener('resize', debouncedCalculate);
 
-        return () => {
-            window.removeEventListener('resize', debouncedCalculate);
-        };
-    }, [debouncedCalculate]);
+    //     return () => {
+    //         window.removeEventListener('resize', debouncedCalculate);
+    //     };
+    // }, [debouncedCalculate]);
 
     return (
         <>
